kubernetes/kubernetes

add cluster registry

federation/registry/cluster/etcd/etcd_test.go
@@ -1,0 +1,137 @@
+/*
+Copyright 2016 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package etcd
+
+import (
+	"testing"
+
+	"k8s.io/kubernetes/federation/apis/federation"
+	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/api/unversioned"
+	"k8s.io/kubernetes/pkg/fields"
+	"k8s.io/kubernetes/pkg/labels"
+	"k8s.io/kubernetes/pkg/registry/generic"
+	"k8s.io/kubernetes/pkg/registry/registrytest"
+	"k8s.io/kubernetes/pkg/runtime"
+	etcdtesting "k8s.io/kubernetes/pkg/storage/etcd/testing"
+)
+
+func newStorage(t *testing.T) (*REST, *etcdtesting.EtcdTestServer) {
+	etcdStorage, server := registrytest.NewEtcdStorage(t, federation.GroupName)
+	restOptions := generic.RESTOptions{etcdStorage, generic.UndecoratedStorage, 1}
+	storage, _ := NewREST(restOptions)
+	return storage, server
+}
+
+func validNewCluster() *federation.Cluster {
+	return &federation.Cluster{
+		ObjectMeta: api.ObjectMeta{
+			Name: "foo",
+			Labels: map[string]string{
+				"name": "foo",
+			},
+		},
+		Spec: federation.ClusterSpec{
+			ServerAddressByClientCIDRs: []unversioned.ServerAddressByClientCIDR{
+				{
+					ClientCIDR:    "0.0.0.0",
+					ServerAddress: "localhost:8888",
+				},
+			},
+		},
+		Status: federation.ClusterStatus{
+			Phase: federation.ClusterPending,
+		},
+	}
+}
+
+func TestCreate(t *testing.T) {
+	storage, server := newStorage(t)
+	defer server.Terminate(t)
+	test := registrytest.New(t, storage.Etcd).ClusterScope()
+	cluster := validNewCluster()
+	cluster.ObjectMeta = api.ObjectMeta{GenerateName: "foo"}
+	test.TestCreate(
+		cluster,
+		&federation.Cluster{
+			ObjectMeta: api.ObjectMeta{Name: "-a123-a_"},
+		},
+	)
+}
+
+func TestUpdate(t *testing.T) {
+	storage, server := newStorage(t)
+	defer server.Terminate(t)
+	test := registrytest.New(t, storage.Etcd).ClusterScope()
+	test.TestUpdate(
+		// valid
+		validNewCluster(),
+		// updateFunc
+		func(obj runtime.Object) runtime.Object {
+			object := obj.(*federation.Cluster)
+			object.Spec.Credential = "bar"
+			return object
+		},
+	)
+}
+
+func TestDelete(t *testing.T) {
+	storage, server := newStorage(t)
+	defer server.Terminate(t)
+	test := registrytest.New(t, storage.Etcd).ClusterScope().ReturnDeletedObject()
+	test.TestDelete(validNewCluster())
+}
+
+func TestGet(t *testing.T) {
+	storage, server := newStorage(t)
+	defer server.Terminate(t)
+	test := registrytest.New(t, storage.Etcd).ClusterScope()
+	test.TestGet(validNewCluster())
+}
+
+func TestList(t *testing.T) {
+	storage, server := newStorage(t)
+	defer server.Terminate(t)
+	test := registrytest.New(t, storage.Etcd).ClusterScope()
+	test.TestList(validNewCluster())
+}
+
+func TestWatch(t *testing.T) {
+	storage, server := newStorage(t)
+	defer server.Terminate(t)
+	test := registrytest.New(t, storage.Etcd).ClusterScope()
+	test.TestWatch(
+		validNewCluster(),
+		// matching labels
+		[]labels.Set{
+			{"name": "foo"},
+		},
+		// not matching labels
+		[]labels.Set{
+			{"name": "bar"},
+			{"foo": "bar"},
+		},
+		// matching fields
+		[]fields.Set{
+			{"metadata.name": "foo"},
+		},
+		// not matchin fields
+		[]fields.Set{
+			{"metadata.name": "bar"},
+		},
+	)
+}

federation/registry/cluster/strategy_test.go
@@ -1,0 +1,157 @@
+/*
+Copyright 2016 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cluster
+
+import (
+	"testing"
+
+	"k8s.io/kubernetes/federation/apis/federation"
+	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/api/testapi"
+	apitesting "k8s.io/kubernetes/pkg/api/testing"
+	"k8s.io/kubernetes/pkg/api/unversioned"
+	"k8s.io/kubernetes/pkg/fields"
+	"k8s.io/kubernetes/pkg/labels"
+	"reflect"
+)
+
+func validNewCluster() *federation.Cluster {
+	return &federation.Cluster{
+		ObjectMeta: api.ObjectMeta{
+			Name:            "foo",
+			ResourceVersion: "4",
+			Labels: map[string]string{
+				"name": "foo",
+			},
+		},
+		Spec: federation.ClusterSpec{
+			ServerAddressByClientCIDRs: []unversioned.ServerAddressByClientCIDR{
+				{
+					ClientCIDR:    "0.0.0.0",
+					ServerAddress: "localhost:8888",
+				},
+			},
+		},
+		Status: federation.ClusterStatus{
+			Phase: federation.ClusterTerminated,
+		},
+	}
+}
+
+func invalidNewCluster() *federation.Cluster {
+	return &federation.Cluster{
+		ObjectMeta: api.ObjectMeta{
+			Name:            "foo",
+			ResourceVersion: "5",
+		},
+		Spec: federation.ClusterSpec{
+			Credential: "bar",
+		},
+		Status: federation.ClusterStatus{
+			Phase: federation.ClusterOffline,
+		},
+	}
+}
+
+func TestClusterStrategy(t *testing.T) {
+	ctx := api.NewDefaultContext()
+	if Strategy.NamespaceScoped() {
+		t.Errorf("Cluster should not be namespace scoped")
+	}
+	if Strategy.AllowCreateOnUpdate() {
+		t.Errorf("Cluster should not allow create on update")
+	}
+
+	cluster := validNewCluster()
+	Strategy.PrepareForCreate(cluster)
+	if cluster.Status.Phase != federation.ClusterPending {
+		t.Errorf("Cluster should not allow setting phase on create")
+	}
+	errs := Strategy.Validate(ctx, cluster)
+	if len(errs) != 0 {
+		t.Errorf("Unexpected error validating %v", errs)
+	}
+
+	invalidCluster := invalidNewCluster()
+	Strategy.PrepareForUpdate(invalidCluster, cluster)
+	if reflect.DeepEqual(invalidCluster.Spec, cluster.Spec) ||
+		!reflect.DeepEqual(invalidCluster.Status, cluster.Status) {
+		t.Error("Only spec is expected being changed")
+	}
+	errs = Strategy.ValidateUpdate(ctx, invalidCluster, cluster)
+	if len(errs) == 0 {
+		t.Errorf("Expected a validation error")
+	}
+	if cluster.ResourceVersion != "4" {
+		t.Errorf("Incoming resource version on update should not be mutated")
+	}
+}
+
+func TestClusterStatusStrategy(t *testing.T) {
+	ctx := api.NewDefaultContext()
+	if StatusStrategy.NamespaceScoped() {
+		t.Errorf("Cluster should not be namespace scoped")
+	}
+	if StatusStrategy.AllowCreateOnUpdate() {
+		t.Errorf("Cluster should not allow create on update")
+	}
+
+	cluster := validNewCluster()
+	invalidCluster := invalidNewCluster()
+	StatusStrategy.PrepareForUpdate(cluster, invalidCluster)
+	if !reflect.DeepEqual(invalidCluster.Spec, cluster.Spec) ||
+		reflect.DeepEqual(invalidCluster.Status, cluster.Status) {
+		t.Error("Only spec is expected being changed")
+	}
+	errs := Strategy.ValidateUpdate(ctx, invalidCluster, cluster)
+	if len(errs) == 0 {
+		t.Errorf("Expected a validation error")
+	}
+	if cluster.ResourceVersion != "4" {
+		t.Errorf("Incoming resource version on update should not be mutated")
+	}
+}
+
+func TestMatchCluster(t *testing.T) {
+	testFieldMap := map[bool][]fields.Set{
+		true: {
+			{"metadata.name": "foo"},
+		},
+		false: {
+			{"foo": "bar"},
+		},
+	}
+
+	for expectedResult, fieldSet := range testFieldMap {
+		for _, field := range fieldSet {
+			m := MatchCluster(labels.Everything(), field.AsSelector())
+			_, matchesSingle := m.MatchesSingle()
+			if e, a := expectedResult, matchesSingle; e != a {
+				t.Errorf("%+v: expected %v, got %v", fieldSet, e, a)
+			}
+		}
+	}
+}
+
+func TestSelectableFieldLabelConversions(t *testing.T) {
+	apitesting.TestSelectableFieldLabelConversionsOfKind(t,
+		testapi.Federation.GroupVersion().String(),
+		"Cluster",
+		labels.Set(ClusterToSelectableFields(&federation.Cluster{})),
+		nil,
+	)
+}

kubernetes-retired/cluster-registry

registry/cluster/etcd/etcd_test.go
@@ -1,0 +1,140 @@
+/*
+Copyright 2016 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package etcd
+
+import (
+	"testing"
+
+	"k8s.io/kubernetes/federation/apis/federation"
+	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/api/unversioned"
+	"k8s.io/kubernetes/pkg/fields"
+	"k8s.io/kubernetes/pkg/labels"
+	"k8s.io/kubernetes/pkg/registry/generic"
+	"k8s.io/kubernetes/pkg/registry/registrytest"
+	"k8s.io/kubernetes/pkg/runtime"
+	etcdtesting "k8s.io/kubernetes/pkg/storage/etcd/testing"
+)
+
+func newStorage(t *testing.T) (*REST, *etcdtesting.EtcdTestServer) {
+	etcdStorage, server := registrytest.NewEtcdStorage(t, federation.GroupName)
+	restOptions := generic.RESTOptions{
+		Storage:                 etcdStorage,
+		Decorator:               generic.UndecoratedStorage,
+		DeleteCollectionWorkers: 1}
+	storage, _ := NewREST(restOptions)
+	return storage, server
+}
+
+func validNewCluster() *federation.Cluster {
+	return &federation.Cluster{
+		ObjectMeta: api.ObjectMeta{
+			Name: "foo",
+			Labels: map[string]string{
+				"name": "foo",
+			},
+		},
+		Spec: federation.ClusterSpec{
+			ServerAddressByClientCIDRs: []unversioned.ServerAddressByClientCIDR{
+				{
+					ClientCIDR:    "0.0.0.0/0",
+					ServerAddress: "localhost:8888",
+				},
+			},
+		},
+		Status: federation.ClusterStatus{
+			Phase: federation.ClusterPending,
+		},
+	}
+}
+
+func TestCreate(t *testing.T) {
+	storage, server := newStorage(t)
+	defer server.Terminate(t)
+	test := registrytest.New(t, storage.Store).ClusterScope()
+	cluster := validNewCluster()
+	cluster.ObjectMeta = api.ObjectMeta{GenerateName: "foo"}
+	test.TestCreate(
+		cluster,
+		&federation.Cluster{
+			ObjectMeta: api.ObjectMeta{Name: "-a123-a_"},
+		},
+	)
+}
+
+func TestUpdate(t *testing.T) {
+	storage, server := newStorage(t)
+	defer server.Terminate(t)
+	test := registrytest.New(t, storage.Store).ClusterScope()
+	test.TestUpdate(
+		// valid
+		validNewCluster(),
+		// updateFunc
+		func(obj runtime.Object) runtime.Object {
+			object := obj.(*federation.Cluster)
+			object.Spec.Credential = "bar"
+			return object
+		},
+	)
+}
+
+func TestDelete(t *testing.T) {
+	storage, server := newStorage(t)
+	defer server.Terminate(t)
+	test := registrytest.New(t, storage.Store).ClusterScope().ReturnDeletedObject()
+	test.TestDelete(validNewCluster())
+}
+
+func TestGet(t *testing.T) {
+	storage, server := newStorage(t)
+	defer server.Terminate(t)
+	test := registrytest.New(t, storage.Store).ClusterScope()
+	test.TestGet(validNewCluster())
+}
+
+func TestList(t *testing.T) {
+	storage, server := newStorage(t)
+	defer server.Terminate(t)
+	test := registrytest.New(t, storage.Store).ClusterScope()
+	test.TestList(validNewCluster())
+}
+
+func TestWatch(t *testing.T) {
+	storage, server := newStorage(t)
+	defer server.Terminate(t)
+	test := registrytest.New(t, storage.Store).ClusterScope()
+	test.TestWatch(
+		validNewCluster(),
+		// matching labels
+		[]labels.Set{
+			{"name": "foo"},
+		},
+		// not matching labels
+		[]labels.Set{
+			{"name": "bar"},
+			{"foo": "bar"},
+		},
+		// matching fields
+		[]fields.Set{
+			{"metadata.name": "foo"},
+		},
+		// not matching fields
+		[]fields.Set{
+			{"metadata.name": "bar"},
+		},
+	)
+}

registry/cluster/strategy_test.go
@@ -1,0 +1,157 @@
+/*
+Copyright 2016 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cluster
+
+import (
+	"testing"
+
+	"k8s.io/kubernetes/federation/apis/federation"
+	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/api/testapi"
+	apitesting "k8s.io/kubernetes/pkg/api/testing"
+	"k8s.io/kubernetes/pkg/api/unversioned"
+	"k8s.io/kubernetes/pkg/fields"
+	"k8s.io/kubernetes/pkg/labels"
+	"reflect"
+)
+
+func validNewCluster() *federation.Cluster {
+	return &federation.Cluster{
+		ObjectMeta: api.ObjectMeta{
+			Name:            "foo",
+			ResourceVersion: "4",
+			Labels: map[string]string{
+				"name": "foo",
+			},
+		},
+		Spec: federation.ClusterSpec{
+			ServerAddressByClientCIDRs: []unversioned.ServerAddressByClientCIDR{
+				{
+					ClientCIDR:    "0.0.0.0/0",
+					ServerAddress: "localhost:8888",
+				},
+			},
+		},
+		Status: federation.ClusterStatus{
+			Phase: federation.ClusterTerminated,
+		},
+	}
+}
+
+func invalidNewCluster() *federation.Cluster {
+	return &federation.Cluster{
+		ObjectMeta: api.ObjectMeta{
+			Name:            "foo2",
+			ResourceVersion: "5",
+		},
+		Spec: federation.ClusterSpec{
+			Credential: "bar",
+		},
+		Status: federation.ClusterStatus{
+			Phase: federation.ClusterPending,
+		},
+	}
+}
+
+func TestClusterStrategy(t *testing.T) {
+	ctx := api.NewDefaultContext()
+	if Strategy.NamespaceScoped() {
+		t.Errorf("Cluster should not be namespace scoped")
+	}
+	if Strategy.AllowCreateOnUpdate() {
+		t.Errorf("Cluster should not allow create on update")
+	}
+
+	cluster := validNewCluster()
+	Strategy.PrepareForCreate(cluster)
+	if len(cluster.Status.Phase) != 0 {
+		t.Errorf("Cluster should not allow setting phase on create")
+	}
+	errs := Strategy.Validate(ctx, cluster)
+	if len(errs) != 0 {
+		t.Errorf("Unexpected error validating %v", errs)
+	}
+
+	invalidCluster := invalidNewCluster()
+	Strategy.PrepareForUpdate(invalidCluster, cluster)
+	if reflect.DeepEqual(invalidCluster.Spec, cluster.Spec) ||
+		!reflect.DeepEqual(invalidCluster.Status, cluster.Status) {
+		t.Error("Only spec is expected being changed")
+	}
+	errs = Strategy.ValidateUpdate(ctx, invalidCluster, cluster)
+	if len(errs) == 0 {
+		t.Errorf("Expected a validation error")
+	}
+	if cluster.ResourceVersion != "4" {
+		t.Errorf("Incoming resource version on update should not be mutated")
+	}
+}
+
+func TestClusterStatusStrategy(t *testing.T) {
+	ctx := api.NewDefaultContext()
+	if StatusStrategy.NamespaceScoped() {
+		t.Errorf("Cluster should not be namespace scoped")
+	}
+	if StatusStrategy.AllowCreateOnUpdate() {
+		t.Errorf("Cluster should not allow create on update")
+	}
+
+	cluster := validNewCluster()
+	invalidCluster := invalidNewCluster()
+	StatusStrategy.PrepareForUpdate(cluster, invalidCluster)
+	if !reflect.DeepEqual(invalidCluster.Spec, cluster.Spec) ||
+		reflect.DeepEqual(invalidCluster.Status, cluster.Status) {
+		t.Error("Only spec is expected being changed")
+	}
+	errs := Strategy.ValidateUpdate(ctx, invalidCluster, cluster)
+	if len(errs) == 0 {
+		t.Errorf("Expected a validation error")
+	}
+	if cluster.ResourceVersion != "4" {
+		t.Errorf("Incoming resource version on update should not be mutated")
+	}
+}
+
+func TestMatchCluster(t *testing.T) {
+	testFieldMap := map[bool][]fields.Set{
+		true: {
+			{"metadata.name": "foo"},
+		},
+		false: {
+			{"foo": "bar"},
+		},
+	}
+
+	for expectedResult, fieldSet := range testFieldMap {
+		for _, field := range fieldSet {
+			m := MatchCluster(labels.Everything(), field.AsSelector())
+			_, matchesSingle := m.MatchesSingle()
+			if e, a := expectedResult, matchesSingle; e != a {
+				t.Errorf("%+v: expected %v, got %v", fieldSet, e, a)
+			}
+		}
+	}
+}
+
+func TestSelectableFieldLabelConversions(t *testing.T) {
+	apitesting.TestSelectableFieldLabelConversionsOfKind(t,
+		testapi.Federation.GroupVersion().String(),
+		"Cluster",
+		labels.Set(ClusterToSelectableFields(&federation.Cluster{})),
+		nil,
+	)
+}