61481325b028b716bbecfc939d734614b839d0e9
Maru Newby <marun@redhat.com>
2018-01-30 09:14:23 -0800

    Update integration package name to match path

--- test/integration/doc.go
+++ test/integration/doc.go
@@ -14,6 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package testing contains an integration test harness and an integration test
+// Package integration contains an integration test harness and an integration test
 // for the cluster registry API server.
-package testing
+package integration
