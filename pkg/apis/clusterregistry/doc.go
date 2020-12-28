39fffb8ace1dda213e5be1fd9c0ac5351452b95b
Leonardo Lima <leonardoaugusto287@gmail.com>
2020-12-28 16:48:30 -0300

    Add missing package comment

--- pkg/apis/clusterregistry/doc.go
+++ pkg/apis/clusterregistry/doc.go
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// Package clusterregistry :
 // +k8s:deepcopy-gen=package,register
 // +groupName=clusterregistry.k8s.io
 package clusterregistry
